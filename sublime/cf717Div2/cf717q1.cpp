99071c8611aa8436ba00a1dea00cbd670d56875e
samirsharma-github <samirsharma.iitd@gmail.com>
2021-04-21 22:21:21 +0530

    cf717

---
+++ sublime/cf717Div2/cf717q1.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const ll mod  = 1e9+7;
+const ld eps  = 1e-9 ;
+const ll maxn = 1e5+1;
+const ll inf  = 1e15 ;
+const ll minf = -inf ;
+
+int main(){
+	//     cout << a + b << '\n';
+	int t;
+	cin>>t;
+	for (int i = 0; i < t; ++i)
+	{
+		int n,k;
+		cin>>n>>k;
+		int a[n];
+		for (int j = 0; j < n; ++j)
+		{	
+			int num;
+			cin>>num;
+			a[j]=num;
+
+		}
+		for (int j = 0; j < n; ++j)
+		{	
+			if (j==n-1)
+			{
+				cout<<a[j];
+				break;
+			}
+			if (a[j]<=k)
+			{
+				k -=a[j];
+				a[n-1]+=a[j];
+				a[j]=0;
+
+			}
+			else
+			{
+				a[n-1]+=k;
+				a[j]-=k;
+				k=0;
+			}
+			cout<<a[j]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
